b74c9cfd66d878b34d08c84cb3f928f506203ce3
kup21 <kup21@ic.ac.uk>
2024-04-17 16:35:21 +0100

    kup: <refact> Refcctored 1 -> made the filter in line with the lost comprehension

--- 1964_solution.py
+++ 1964_solution.py
@@ -6,7 +6,7 @@ student_id = "1964"
 ### Do not change the variable names above, just fill them in ###
 
 from dataclasses import dataclass
-from typing import Optional, TypeAlias, Any
+from typing import Literal, Optional, TypeAlias, Any
 from zeep import Client
 import json
 from pydantic import BaseModel  
@@ -18,30 +18,54 @@ class Duyuru(BaseModel):
   TIP: str 
   GUNCELLEME_SAATI: str
   MESAJ: str
+  
+class FiloArac(BaseModel):
+  Operator: str
+  Garaj: Optional[str]
+  KapiNo: str
+  Saat: str ## make DateTime??
+  Boylam: float
+  Enlem: float
+  Hiz: int ## float??
+  Plaka: Optional[str] ## plaka class??
 
 def announcements(line_code: str) -> tuple[int, list[str]]:
     duyuruUrl = "https://api.ibb.gov.tr/iett/UlasimDinamikVeri/Duyurular.asmx?wsdl"
-    DuyuruClient = Client(wsdl = duyuruUrl)
-    announcements = DuyuruClient.service.GetDuyurular_json()
-    
+    duyuruClient = Client(wsdl = duyuruUrl)
+    announcements = duyuruClient.service.GetDuyurular_json()
     announcements: list[Duyuru] = [Duyuru.model_validate(d) for d in json.loads(announcements)]
     
-    thisLine = filter(lambda a: a.HATKODU == line_code, announcements)
-    mesajs = [duyuru.MESAJ for duyuru in thisLine]
-    
+    mesajs = [duyuru.MESAJ for duyuru in filter(lambda duyuru: duyuru.HATKODU == line_code, announcements)]
     return len(mesajs), mesajs
     
     
-def stopping_buses():
-    pass
+def stopping_buses() -> list[str]:
+  fleetUrl = "https://api.ibb.gov.tr/iett/FiloDurum/SeferGerceklesme.asmx?wsdl"
+  fleetClient = Client(wsdl = fleetUrl)
+  fleetData = fleetClient.service.GetFiloAracKonum_json()
+  fleetData: list[FiloArac] = [FiloArac.model_validate(fd) for fd in json.loads(fleetData)]
+  
+  ## assumption: find stopping from speed? if hiz == 0 => stopping
+  return [arac.KapiNo for arac in filter(lambda arac: arac.Hiz == 0, fleetData)]
+  
+def max_speeds() -> list[FiloArac.json]: ## in json format
+    fleetUrl = "https://api.ibb.gov.tr/iett/FiloDurum/SeferGerceklesme.asmx?wsdl"
+    fleetClient = Client(wsdl = fleetUrl)
+    fleetData = fleetClient.service.GetFiloAracKonum_json()
+    fleetData: list[FiloArac] = [FiloArac.model_validate(fd) for fd in json.loads(fleetData)]
+    print(f"{ fleetData[0] =}")
     
-def max_speeds():
-    pass
+    return [arac.model_dump() for arac in sorted(fleetData, key = lambda fa: fa.Hiz, reverse = True)[:2]]
     
-def show_line_stops(line_code, direction):
-    pass
+def show_line_stops(line_code: str, direction: Literal["D", "G"]) -> list[str]:
     
 def live_tracking(line_code, direction):
     pass
 
 print(f"{announcements("10") = }")
+print()
+# print(f"{stopping_buses() = }")
+print()
+print(f"{ max_speeds() = }")
+print()
+
