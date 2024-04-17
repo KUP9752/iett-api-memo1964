15b38a6cb47a0d40cc270638ab98dd67acd5730a
kup21 <kup21@ic.ac.uk>
2024-04-17 15:48:10 +0100

    kup: Created the repo and moved the files

---
+++ 1964_solution.py
@@ -0,0 +1,22 @@
+############ Do not change the assignment code value ############
+assignment_code = 140110201
+name = "Baha"
+surname = "Blast"
+student_id = "1964"
+### Do not change the variable names above, just fill them in ###cd 
+
+def announcements(line_code):
+    pass
+    
+def stopping_buses():
+    pass
+    
+def max_speeds():
+    pass
+    
+def show_line_stops(line_code, direction):
+    pass
+    
+def live_tracking(line_code, direction):
+    pass
+
